0efcdc3f6f6c22615162045e072984853ab06c7c
shozanthebozan <shozanthebozan@gmail.com>
2026-07-10 15:02:34 +1000

    Change find command to be case insensitive
    for CLI ver

--- Searchy (v0.0.1).sh
+++ Searchy (v0.0.1).sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "What would you like to search for?: " query
-results=$(find / -name $query 2> /dev/null)
+results=$(find / -iname $query 2> /dev/null)
 if [ -z "$results" ]; then
  echo "File not found: 404"
 else
