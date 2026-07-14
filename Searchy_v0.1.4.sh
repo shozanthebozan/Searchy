4c3233d9c1d3a2335ec78f5eb84370152fa9e08a
shozanthebozan <shozanthebozan@gmail.com>
2026-07-14 22:07:05 +1000

    Saving my progress so far, still very bugged.

--- Searchy_v0.1.4.sh
+++ Searchy_v0.1.4.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 read -p "What would you like to search for?: " query
-results=$(find / \( -path "/tmp" -o -path "/proc" -o -path "/sys" -o \( -path "/run/*" -not -path "/run/media*" \) \) -prune -o -iname "$query" -print 2> /dev/null)
+read -p "Which directory would you like to search in? (default=/): " search_dir
+if [ -z "$search_dir" ]; then
+ search_dir="/"
+else
+ search_dir=$"search_dir"
+fi
+results=$(find "$search_dir" \( -path "/tmp" -o -path "/proc" -o -path "/sys" -o \( -path "/run/*" -not -path "/run/media*" \) \) -prune -o -iname "$query" -print 2> /dev/null)
 if [ -z "$results" ]; then
  echo "File not found: 404"
 else
