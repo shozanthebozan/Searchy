be032be83c3dcbd6fc76789f7c5ad4ffbb4a3087
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 06:58:20 +1000

    Saving again

--- Searchy_v0.1.4.sh
+++ Searchy_v0.1.4.sh
@@ -2,9 +2,9 @@
 read -p "What would you like to search for?: " query
 read -p "Which directory would you like to search in? (default=/): " search_dir
 if [ -z "$search_dir" ]; then
- search_dir="/"
+ search_dir=/
 else
- search_dir=$"search_dir"
+ search_dir="$search_dir"
 fi
 results=$(find "$search_dir" \( -path "/tmp" -o -path "/proc" -o -path "/sys" -o \( -path "/run/*" -not -path "/run/media*" \) \) -prune -o -iname "$query" -print 2> /dev/null)
 if [ -z "$results" ]; then
